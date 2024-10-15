8e74411d4d13ae6b8dab5b93e40007d965be37f4
PrashanthSai-K <saiprashanth.cs21@bitsathy.ac.in>
2024-10-15 14:48:45 +0530

    Project Structure dev

--- api/main.go
+++ api/main.go
@@ -3,27 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/PrashanthSai-K/DeployGenie/api/database"
-	"github.com/PrashanthSai-K/DeployGenie/api/initial"
-	"github.com/PrashanthSai-K/DeployGenie/api/router"
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 
-	fmt.Println("Helloworld...!!!")
+	fmt.Println("Helloworlddd...!!!")
 
-	app := fiber.New()
-	app.Use(cors.New())
-
-	database.ConnectDB()
-
-	initial.InitializeDB()
-
-	initial.InitializeImages()
-	
-	router.SetUpRoutes(app)	
-
-	app.Listen(":3500")
 }
